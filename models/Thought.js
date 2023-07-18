0a4ad466a83c007390d407899ca1f84048ecf4ae
carolinebea <coraline.bea@gmail.com>
2023-07-17 21:53:15 -0400

    added users

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
